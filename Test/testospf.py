219cc3cd343981482957a7fed06d98f1d306980d
DataKnox <knox@knoxsdata.com>
2020-11-03 23:21:03 -0600

    false test

--- Test/testospf.py
+++ Test/testospf.py
@@ -43,7 +43,7 @@ def test_ospf():
                         ospf_state = True
                         print(True)
         # print(new_url)
-        assert(ospf_state == True)
+        assert(ospf_state == False)
 
 
 test_ospf()
